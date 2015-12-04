32992a95a3e856619c704c15e4f70986314b4ed3
Antonio Sanso <antonio.sanso@gmail.com>
2015-12-04 17:36:01 +0100

    Update helloWorldJson.js

--- helloWorldJson.js
+++ helloWorldJson.js
@@ -7,13 +7,33 @@ var resource = {
 };
 
 app.get("/", function(req, res){
-  var name = req.query.name;
-  resource.helloWorld = name;
 
-  res.send('{"helloWorld": "'+name+'"}');
-  //res.json(resource);
-
+  if (req.query.name) {	
+    nosql.insert(req.query.name);
+      res.writeHead(302,
+     {Location: '/'}
+    );
+    res.end();
+  } else if (req.query.surname) {
+  	 resource.helloWorld = req.query.surname;
+	   res.json(resource);
+  } else if (req.query.title) {
+  	 res.send('{"helloWorld": "'+req.query.title+'"}');
+  } else {
+    nosql.one(function(name) {
+        return name; ;
+      },function(err, name) {
+    if (name) {      
+      nosql.clear();
+      res.send('{"helloWorld": "'+name+'"}');
+      return;
+    } else {       
+      res.send('{"helloWorld"}');
+      return;
+    }});
+  }
 });
+
 
 var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
